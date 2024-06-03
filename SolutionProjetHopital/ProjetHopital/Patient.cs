a131cfbc424866b33bc2e39b03fd55003f95f1a7
benjamin <ben.hervouet@gmail.com>
2024-06-03 13:31:06 +0200

    création des class Visite, Salle, Medecin, Patient

---
+++ SolutionProjetHopital/ProjetHopital/Patient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetHopital
+{
+    class Patient
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private int age;
+        private string adresse;
+        private string telephone;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int Age { get => age; set => age = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string Telephone { get => telephone; set => telephone = value; }
+
+        public Patient(int id, string nom, string prenom, int age, string adresse, string telephone)
+        {
+            this.Id = id;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Age = age;
+            this.Adresse = adresse;
+            this.Telephone = telephone;
+        }
+    }
+}
--- SolutionProjetHopital/ProjetHopital/Program.cs
+++ SolutionProjetHopital/ProjetHopital/Program.cs
@@ -10,7 +10,7 @@ namespace ProjetHopital
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            
         }
     }
 }
